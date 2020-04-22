7c8b1411e3bf56a90c9656d5afab512138c40299
Noam Astary <astar28@gmail.com>
2020-04-22 13:21:23 +0300

    Change webpack settings

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
+import {devServer} from "vuetify-loader/dev/webpack.config";
 
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ]
+}
+devServer = {
+  disableHostCheck: true,
+      host: '0.0.0.0',
+      port: 3000
 }
