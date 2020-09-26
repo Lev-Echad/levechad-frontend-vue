95b8045f1fc01cb0bbde3ef784334b5da7d76b11
YG <yg@yg.yg>
2020-09-26 18:06:05 +0300

    mission ops ui improvement

--- src/views/hamal/components/market/MissionDashboard/Mission.vue
+++ src/views/hamal/components/market/MissionDashboard/Mission.vue
@@ -1,162 +1,213 @@
 <template>
-  <v-row dense>
-    <v-col  text-center cols="12">
-      <v-card class="text-center white--text" :color="getStatusColor(focusedMission.status)">
-          <p class="ma-0"> משימה מספר {{ focusedMission.id }}  </p>
-          <strong>{{ focusedMission.status }}</strong>
-      </v-card>
-    </v-col>
-    <v-col cols="12">
+    <v-card>
 
-      <v-card class="indigo lighten-2 text-center white--text">
-        <strong>{{ focusedMission.full_name }}</strong>
-      </v-card>
-    </v-col>
-    <v-col cols="12">
-      <v-card class="indigo lighten-2 text-center white--text">
-        <strong><span>{{moment(focusedMission.created_date).format('DD-MM-YY, h:mm')}}</span>
-</strong>
-      </v-card>
-    </v-col>
-    <v-col cols="12">
-      <v-card class="indigo lighten-2  text-center white--text">
-        <strong v-if="focusedMission.city">עיר: {{
-          focusedMission.city.name
-        }}</strong>
-      </v-card>
-    </v-col>
-    <v-col cols="12">
-      <v-card class="indigo lighten-2  text-center white--text">
-        <strong>סוג: {{ focusedMission.type }}</strong>
-      </v-card>
-    </v-col>
-    <v-col cols="12">
-      <v-card class="indigo lighten-2  text-center white--text">
-        {{ focusedMission.phone_number }}
-      </v-card>
-    </v-col>
-    <v-col cols="12">
-      <v-card class="indigo lighten-2  text-center white--text">
-        <p  class="ma-0 pa-0">כתובת: {{ focusedMission.address }}</p>
-      </v-card>
-    </v-col>
-    <v-col cols="12">
-      <v-card class="indigo lighten-2  text-center white--text">
-          <p class="ma-0 pa-0">פירוט בקשה</p>
-          {{ focusedMission.type_text }}
-      </v-card>
-    </v-col>
-    <v-col v-if="focusedMission.helping_volunteer" cols="12">
+        <v-card :color="getStatusColor(focusedMission.status)" class="text-center white--text">
+            <p class="ma-0"> משימה מספר {{ focusedMission.id }} </p>
+            <strong>{{ focusedMission.status }}</strong>
+        </v-card>
+        <v-list class="pa-0" color="grey lighten-4" dense subheader>
 
-      <v-card class="info lighten-2  text-center white--text">
-          <p  class="ma-0 pa-0"> מס׳ מתנדב: {{ focusedMission.helping_volunteer.id }}</p>
-          <p  class="ma-0 pa-0" v-text="focusedMission.helping_volunteer.full_name"></p>
-          <p  class="ma-0 pa-0" v-text="focusedMission.helping_volunteer.phone_number"></p>
-      </v-card>
-    </v-col>
-    <v-divider> </v-divider>
-    <v-row>
-      <v-col cols="12">
-        <v-radio-group v-model="HamalHelpRequestFields.status">
-          <v-radio label="התקבלה" value="WAITING"></v-radio>
-          <v-radio color="orange" label="בטיפול חמל" value="IN_CARE"></v-radio>
-          <v-radio color="green" label="הועבר למתנדב" value="TO_VOLUNTER"></v-radio>
-          <v-radio color="info" label="סיום טיפול" value="DONE"></v-radio>
-          <v-radio color="info" label="לא רלוונטי" value="NOT_DONE"></v-radio>
-        </v-radio-group>
-        <v-text-field
-          label="מס' מתנדב"
-          outlined=""
-          hint="יש לעדכן סטטוס להועבר למתנדב"
-          :persistent-hint="(HamalHelpRequestFields.status!='TO_VOLUNTER')"
-          v-show="HamalHelpRequestFields.status=='TO_VOLUNTER'"
-          v-model="HamalHelpRequestFields.helping_volunteer"
-        >
-        </v-text-field>
-        <v-textarea
-          label='הערות חמ"ל'
-          v-model="HamalHelpRequestFields.notes"
-          class="py-2"
-          outlined=""
-          hint="דברים שקשורים לפניה"
-        >
-        </v-textarea>
-        <v-text-field
-        outlined=""
-        v-model="HamalHelpRequestFields.status_updater"
-        label="חמליסט מטפל"
-        >
-        </v-text-field>
-      </v-col>
-      <v-col cols="12">
-          <p v-show="!HamalHelpRequestFields.helping_volunteer && (HamalHelpRequestFields.status=='TO_VOLUNTER')" class="primary--text">יש לעדכן מספר מתנדב לפני שמירה!</p>
-        <v-btn
+            <v-subheader class="black--text">פרטי הבקשה</v-subheader>
 
-                :disabled="!HamalHelpRequestFields.helping_volunteer && (HamalHelpRequestFields.status=='TO_VOLUNTER')"
-                raised class="success" @click="saveChanges()">
-          שמירה
-        </v-btn>
-      </v-col>
-      <v-col cols="12"> </v-col>
-      <v-col cols="12"> </v-col>
-    </v-row>
-  </v-row>
+            <v-list-item class="px-1">
+                <v-list-item-icon>
+                    <v-icon>mdi-calendar-clock</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content class="caption">{{moment(focusedMission.created_date).format('DD/MM/YY, hh:mm')}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item class="px-1">
+                <v-list-item-icon>
+                    <v-icon>mdi-account</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>{{focusedMission.full_name}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item class="px-1">
+                <v-list-item-icon>
+                    <v-icon>mdi-phone</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>{{focusedMission.phone_number}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item class="px-1" v-if="focusedMission.city">
+                <v-list-item-icon>
+                    <v-icon>mdi-city</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>{{focusedMission.city.name}}</v-list-item-content>
+            </v-list-item>
+
+            <v-list-item class="px-1">
+                <v-list-item-icon>
+                    <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>{{focusedMission.address}}</v-list-item-content>
+            </v-list-item>
+
+            <v-list-item class="px-1">
+                <v-list-item-icon>
+                    <v-icon>mdi-hand</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>{{focusedMission.type}}</v-list-item-content>
+            </v-list-item>
+        </v-list>
+
+
+        <v-col class="pa-0 ma-0" cols="12">
+            <v-card class="ma-0 elevation-0 caption" color="grey lighten-4">
+                {{ focusedMission.type_text }}
+            </v-card>
+        </v-col>
+        <v-divider></v-divider>
+        <v-list class="caption" color="light-green" dense subheader v-if="focusedMission.helping_volunteer">
+            <v-subheader class="black--text">פרטי מתנדב שהוקצה</v-subheader>
+            <v-list-item class="px-1">
+                <v-list-item-icon>
+                    <v-icon>mdi-card-account-details-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>מס' מתנדב: {{ focusedMission.helping_volunteer.id}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item class="px-1">
+                <v-list-item-icon>
+                    <v-icon>mdi-account-check</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>{{focusedMission.helping_volunteer.full_name}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item class="px-1">
+                <v-list-item-icon>
+                    <v-icon>mdi-phone</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>{{focusedMission.helping_volunteer.phone_number}}</v-list-item-content>
+            </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-row class="my-0 mx-0">
+            <v-list class="py-0 my-0" dense subheader>
+                <v-subheader class="black--text py-0">עדכון משימה</v-subheader>
+            </v-list>
+            <v-col class="py-0" cols="12">
+                <v-select color="indigo" :items="status_items" label="סטטוס" v-model="HamalHelpRequestFields.status"></v-select>
+
+                <v-text-field
+                        label="מס' מתנדב"
+                        outlined=""
+                        color="indigo"
+                        v-model="HamalHelpRequestFields.helping_volunteer"
+                        v-show="HamalHelpRequestFields.status=='TO_VOLUNTER'"
+                >
+                </v-text-field>
+                <v-textarea
+                        class="pt-2"
+                        hint="דברים שקשורים לפניה"
+                        label='הערות חמ"ל'
+                        outlined=""
+                        color="indigo"
+                        v-model="HamalHelpRequestFields.notes"
+                >
+                </v-textarea>
+                <v-text-field
+                        label="חמליסט מטפל"
+                        outlined=""
+                        color="indigo"
+                        v-model="HamalHelpRequestFields.status_updater"
+                >
+                </v-text-field>
+            </v-col>
+            <v-col cols="12">
+                <p class="primary--text"
+                   v-show="!HamalHelpRequestFields.helping_volunteer && (HamalHelpRequestFields.status=='TO_VOLUNTER')">
+                    יש לעדכן מספר מתנדב לפני שמירה!</p>
+                <v-btn
+
+                        :disabled="!HamalHelpRequestFields.helping_volunteer && (HamalHelpRequestFields.status=='TO_VOLUNTER')"
+                        @click="saveChanges()" class="success" raised>
+                    שמירה
+                </v-btn>
+            </v-col>
+        </v-row>
+
+    </v-card>
+
 </template>
 
 <script>
 
-export default {
-
-  computed: {
-    focusedMission: {
-      get() {
-        return this.$store.getters["market/currentHelpRequest"];
-      },
-    },
-      helpingVolunteerDetails(){
-       return this.focusedMission.helping_volunteer
-      },
-    isLoading() {
-      return this.$store.getters["match/isLoading"];
-    },
-    HamalHelpRequestFields() {
-      return this.$store.getters["market/HamalHelpRequestFields"];
-    },
-
-  },
-
-  mounted(){
-  // this.$store.commit("market/currentHelpRequestHamalFields", this.focusedMission)
-    // if(this.focusedMission.helping_volunteer){
-    //     this.$store.dispatch("hamalVolunteers/reqVolunteerDetailsById", this.focusedMission.helping_volunteer.id)
-    // }
-  },
-  methods: {
-
-    saveChanges() {
-      this.$store.dispatch("market/updateHelpRequest");
-    },
-    getStatusColor(status) {
-      let color = "white";
-      switch (status) {
-        case "התקבלה":
-          color = "red";
-          break;
-        case "בטיפול":
-          color = "orange";
-          break;
-        case "הועבר למתנדב":
-          color = "green";
-          break;
-        case "טופל":
-          color = "info";
-          break;
-        case "לא טופל":
-          color = "red";
-          break;
+  export default {
+    data() {
+      return {
+        status_items: [
+          {
+            value: "WAITING",
+            text: "התקבלה",
+          },
+          {
+            value: "IN_CARE",
+            text: "בטיפול חמל",
+          },
+          {
+            value: "TO_VOLUNTER",
+            text: "הועבר למתנדב",
+          },
+          {
+            value: "DONE",
+            text: "בוצעה",
+          },
+          {
+            value: "NOT_DONE",
+            text: "לא רלוונטית",
+          }
+        ]
       }
-      return color;
     },
-  },
-};
+    computed: {
+      focusedMission: {
+        get() {
+          return this.$store.getters["market/currentHelpRequest"];
+        },
+      },
+      helpingVolunteerDetails() {
+        return this.focusedMission.helping_volunteer
+      },
+      isLoading() {
+        return this.$store.getters["match/isLoading"];
+      },
+      HamalHelpRequestFields() {
+        return this.$store.getters["market/HamalHelpRequestFields"];
+      },
+
+    },
+
+    mounted() {
+      // this.$store.commit("market/currentHelpRequestHamalFields", this.focusedMission)
+      // if(this.focusedMission.helping_volunteer){
+      //     this.$store.dispatch("hamalVolunteers/reqVolunteerDetailsById", this.focusedMission.helping_volunteer.id)
+      // }
+    },
+    methods: {
+
+      saveChanges() {
+        this.$store.dispatch("market/updateHelpRequest");
+      },
+      getStatusColor(status) {
+        let color = "white";
+        switch (status) {
+          case "התקבלה":
+            color = "red";
+            break;
+          case "בטיפול":
+            color = "orange";
+            break;
+          case "הועבר למתנדב":
+            color = "green";
+            break;
+          case "טופל":
+            color = "info";
+            break;
+          case "לא טופל":
+            color = "red";
+            break;
+        }
+        return color;
+      },
+    },
+  };
 </script>
