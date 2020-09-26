029ea854fd5d2811b822729c7122fc03f797fa03
YG <yg@yg.yg>
2020-09-26 19:34:02 +0300

    change pagesize to max (100) for open missions

--- src/store/modules/market.js
+++ src/store/modules/market.js
@@ -136,7 +136,7 @@ const actions = {
   reqOpenHelpRequests(context) {
     context.commit("isLoading", true);
     axios
-    .get(context.rootState.baseAPIurl + "/api/helprequests/?status__in=IN_CARE,WAITING,TO_VOLUNTER&pageSize=60", {
+    .get(context.rootState.baseAPIurl + "/api/helprequests/?status__in=IN_CARE,WAITING,TO_VOLUNTER&pageSize=100", {
       headers: {
         Authorization: "Token " + context.rootState.hamalAuth.accessToken,
       },
