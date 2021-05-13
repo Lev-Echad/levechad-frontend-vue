d38ff4509c773fe05043916367828700745dda9f
thereisnospoon <segal.guy@gmail.com>
2021-05-13 17:41:18 +0300

    Redirecting to Monday registration form instead of corona form

--- src/views/volunteer/Auth/Signup.vue
+++ src/views/volunteer/Auth/Signup.vue
@@ -14,29 +14,30 @@
 
 
 <script>
-import OneForm from "./Form"
-import Terms from "./components/forms/Terms"
+window.location.href = 'https://forms.monday.com/forms/55e180ed56b58dd4244c3d0c1b9104ff?r=use1';
+// import OneForm from "./Form"
+// import Terms from "./components/forms/Terms"
 
-export default {
-  components:{
-     OneForm, Terms
-  },
-  data(){
-    return{
+// export default {
+//   components:{
+//      OneForm, Terms
+//   },
+//   data(){
+//     return{
 
-      isAcceptTerms: false
-    }
-  },
-  methods:{
-    acceptedTerms(){
-    this.$vuetify.goTo(this.$refs.OneForm)
-    this.isAcceptTerms = true
-    }
+//       isAcceptTerms: false
+//     }
+//   },
+//   methods:{
+//     acceptedTerms(){
+//     this.$vuetify.goTo(this.$refs.OneForm)
+//     this.isAcceptTerms = true
+//     }
 
-  },
-  mounted(){
-    this.$vuetify.goTo(this.$refs.Terms)
-  }
+//   },
+//   mounted(){
+//     this.$vuetify.goTo(this.$refs.Terms)
+//   }
   
-}
+// }
 </script>
